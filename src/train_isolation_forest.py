b92d08dbdd38cbbe7ddc2dadd367a11880e1e7a8
shravanichavan768-alt <shravanichavan768@gmail.com>
2026-05-02 02:35:53 +0530

    update isolation forest module for new insurance dataset

--- src/train_isolation_forest.py
+++ src/train_isolation_forest.py
@@ -1,15 +1,14 @@
-
+# src/train_isolation_forest.py
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.ensemble import IsolationForest
 from sklearn.metrics import (classification_report, confusion_matrix,
-                             roc_auc_score, roc_curve)
+                             roc_auc_score)
 import joblib
 import os
 
-def train_isolation_forest(X_train, contamination=0.07):
+def train_isolation_forest(X_train, contamination=0.13):
     iso_forest = IsolationForest(
         n_estimators=100,
         contamination=contamination,
@@ -28,7 +27,6 @@ def evaluate_isolation_forest(iso_forest, X_test, y_test):
     print(classification_report(y_test, iso_pred,
           target_names=['Legitimate', 'Fraud']))
 
-    # Confusion Matrix
     os.makedirs('outputs', exist_ok=True)
     cm = confusion_matrix(y_test, iso_pred)
     plt.figure(figsize=(6, 5))
@@ -49,11 +47,11 @@ def evaluate_isolation_forest(iso_forest, X_test, y_test):
 def save_isolation_forest(model, path='models/isolation_forest.pkl'):
     os.makedirs('models', exist_ok=True)
     joblib.dump(model, path)
-    print(f"Isolation Forest saved to {path} ")
+    print(f"Isolation Forest saved ")
 
 def load_isolation_forest(path='models/isolation_forest.pkl'):
     model = joblib.load(path)
-    print(f"Isolation Forest loaded from {path} ")
+    print(f"Isolation Forest loaded ")
     return model
 
 if __name__ == "__main__":
